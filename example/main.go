905cde8994598b04dbc5be71115d19877d65ea14
jinzhongwei <jinzhongwei@didichuxing.com>
2018-12-26 18:47:32 +0800

    init

---
+++ example/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	//"time"
+
+	"github.com/kamildrazkiewicz/go-flow"
+)
+
+func main() {
+	f1 := func(r map[string]interface{}) (interface{}, error) {
+		fmt.Println("function1 started")
+		return 1, nil
+	}
+
+	f2 := func(r map[string]interface{}) (interface{}, error) {
+		fmt.Println("function2 started", r["f1"])
+		return "some results", nil // errors.New("Some error")
+	}
+
+	f3 := func(r map[string]interface{}) (interface{}, error) {
+		fmt.Println("function3 started", r["f1"])
+		return nil, nil
+	}
+
+	f4 := func(r map[string]interface{}) (interface{}, error) {
+		fmt.Println("function4 started", r)
+		return nil, nil
+	}
+
+	res, err := goflow.New().
+		Add("f1", nil, f1).
+		Add("f2", []string{"f1"}, f2).
+		Add("f3", []string{"f1"}, f3).
+		Add("f4", []string{"f2", "f3"}, f4).
+		Do()
+
+	fmt.Println(res, err)
+}
